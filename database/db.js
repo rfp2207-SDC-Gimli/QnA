687b20411dd379ced50185a24f13d1d26b22be18
Aaron Yabut <aaronjiego@yahoo.com>
2022-09-21 13:03:15 -0700

    Loader IO

--- database/db.js
+++ database/db.js
@@ -1,13 +1,6 @@
 const { Pool, Client } = require('pg');
 require("dotenv").config();
 
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: process.env.DB_PORT
-// });
 
 const pool = new Pool({
   user: process.env.PGUSER,
@@ -16,6 +9,5 @@ const pool = new Pool({
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT
 });
-
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ app.use('/qa', require('./routes.js'));
 
 const PORT = `${process.env.PORT}` || 3000;
 
+app.get("/loaderio-3c86398cce07b38a22addc325babf818", (req, res) => res.send("loaderio-3c86398cce07b38a22addc325babf818"))
+
 app.listen(PORT, () => {
   console.log(`Listening at localbro:${PORT}`);
   // console.log(`Database: ${process.env.DB_NAME}`);
